331a629113720d56bee052e954962cc7a06f3e68
quyen <nttqa22001@yahoo.com>
2013-04-30 15:39:01 +0200

    fix doi_record to include publication_types

--- lib/doi_record.rb
+++ lib/doi_record.rb
@@ -1,8 +1,10 @@
 class DoiRecord
-  attr_accessor :authors, :title, :abstract, :journal, :doi, :xml, :date_published
-    
+  attr_accessor :authors, :title, :abstract, :journal, :doi, :xml, :date_published,:publication_type, :error
+
+  PUBLICATION_TYPES = {:journal=>1,:conference=>2,:book_chapter=>3}
+
   DOI_BASE_URL = "http://dx.doi.org/"
-  
+
   def initialize(attributes={})
     self.title = attributes[:title]
     self.abstract = attributes[:abstract]
@@ -11,6 +13,8 @@ class DoiRecord
     self.xml = attributes[:doc]
     self.date_published = attributes[:pub_date]
     self.authors = attributes[:authors] || []    
+    self.publication_type = attributes[:type] || PUBLICATION_TYPES[:journal]
+    self.error=attributes[:error] || nil
   end
   
   def lookup_url
