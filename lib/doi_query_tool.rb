339396541cf4bf54081e6004f9dec3ece6ce6b5c
quyen <nttqa22001@yahoo.com>
2013-04-30 15:05:58 +0200

    need to load files inside doi_query_tool.rb

--- lib/doi_query_tool.rb
+++ lib/doi_query_tool.rb
@@ -1,3 +1,7 @@
+require "doi_query.rb"
+require "doi_record.rb"
+require "doi_author.rb"
+
 module DoiQueryTool
   
 end
--- rails/init.rb
+++ rails/init.rb
@@ -1,4 +1,2 @@
 require "doi_query_tool.rb"
-require "doi_query.rb"
-require "doi_record.rb"
-require "doi_author.rb"
+
