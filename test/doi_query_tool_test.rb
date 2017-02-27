f9f4bf8110aa488c77d7693f7da3e1d5bba2c327
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-02-27 15:43:17 +0000

    Added VCRs to git and added a test for pre-print publications

--- test/doi_query_tool_test.rb
+++ test/doi_query_tool_test.rb
@@ -1,16 +1,26 @@
 require 'test_helper'
 
 class DoiQueryToolTest < Test::Unit::TestCase
+  def setup
+    @client = DOI::Query.new('test@localhost') # This is not a working email address
+  end
+
   def test_doi_query
-    VCR.use_cassette('fetch_comp_sci_doi') do
-      email_address = 'methodbox@gmail.com'
-      doi = '10.1007/978-3-540-70504-8_9'
-      query = DOI::Query.new(email_address)
-      result = query.fetch(doi)
-      assert_equal(result.authors.first.first_name, 'Christian Y. A.')
-      assert_equal(result.title, 'A Semantics for a Query Language over Sensors, Streams and Relations')
-      assert_equal(result.authors.size, 4)
-      assert_equal('Lecture Notes in Computer Science 5071 : 87', result.citation)
+    VCR.use_cassette('fetch_comp_sci_book_chapter_doi') do
+      result = @client.fetch('10.1007/978-3-540-70504-8_9')
+      assert_equal 'Christian Y. A.', result.authors.first.first_name
+      assert_equal 'A Semantics for a Query Language over Sensors, Streams and Relations', result.title
+      assert_equal 4, result.authors.size
+      assert_equal 'Lecture Notes in Computer Science 5071 : 87', result.citation
+      assert_equal :book_chapter, result.publication_type
+    end
+  end
+
+  def test_pre_print_doi
+    VCR.use_cassette('fetch_pre_print_doi') do
+      result = @client.fetch('10.1101/105437')
+      assert_equal result.authors.first.first_name, 'Yin Hoon'
+      assert_equal result.publication_type, :pre_print
     end
   end
 end
